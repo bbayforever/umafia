f3d138ee3c2c876c22509ddcb8af303be4a592aa
bbayforever <workworkwork12@gmail.com>
2019-07-24 16:54:01 +0300

    added checkout page, checkout-item component and updated cart to use selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/logo1.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 
 import './header.styles.scss';
 
@@ -37,9 +42,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,7 +4,12 @@ const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
     [selectCart],
-    (cart) => cart.cartItems
+    cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden 
 );
 
 export const selectCartItemsCount = createSelector(
@@ -15,3 +20,13 @@ export const selectCartItemsCount = createSelector(
                 accumalatedQuantity + cartItem.quantity, 0
         )
     );
+
+    export const selectCartItemsTotal = createSelector(
+        [selectCartItems],
+        cartItems =>
+        cartItems.reduce(
+            (accumalatedQuantity, cartItem) => 
+                accumalatedQuantity + cartItem.quantity * cartItem.price, 0
+        )
+
+    )
